8c0625858d32748b404caf0b6170f72c328209d1
hellokitty9 <1836174807@qq.com>
2023-08-09 11:58:00 +0800

    home面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,4 +15,13 @@ import homeProduct from './components/HomeProduct.vue'
     <HomeNew />
     <HomeHot />
     <homeProduct />
+    <!-- 测试面板组件 -->
+    <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+        <div>
+            新鲜好物插槽
+        </div>
+    </HomePanel>
+    <HomePanel title="人气推荐" sub-title="人气推荐 好多商品">
+        <div>人气推荐</div>
+    </HomePanel>
 </template>
