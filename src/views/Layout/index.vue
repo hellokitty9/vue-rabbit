ed168d2062ded0313d02d6756a43b7b895d32901
hellokitty9 <1836174807@qq.com>
2023-08-09 09:53:46 +0800

    pinia优化导航数据

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,6 +3,17 @@ import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 import LayoutFixed from './components/LayoutFixed.vue';
+
+// 触发获取导航列表的action
+import { useCategoryStore } from '@/stores/category'
+import { onMounted } from "vue";
+
+const CategoryStore = useCategoryStore()
+
+onMounted(() => {
+    CategoryStore.getCategory()
+})
+
 </script>
 
 <template>
