718a3d3faaf88cd3353adb6196ee8e4abc0355fb
hellokitty9 <1836174807@qq.com>
2023-08-10 18:47:23 +0800

    home-轮播图实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { getCategoryAPI } from '@/apis/category'
 import { useRoute } from "vue-router";
+import { getBannerAPI } from '@/apis/home'
 import { onMounted, onUpdated, ref } from 'vue'
 
 // 获取数据
@@ -17,6 +18,20 @@ onUpdated(() => {
     getCategory()
 })
 
+// 获取banner
+
+const bannerList = ref([])
+
+const getBanner = async () => {
+    const res = await getBannerAPI({
+        distributionSite: '2'
+    })
+    console.log(res)
+    bannerList.value = res.result
+}
+
+onMounted(() => getBanner())
+
 </script>
 
 <template>
@@ -29,12 +44,31 @@ onUpdated(() => {
                     <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
                 </el-breadcrumb>
             </div>
+            <!-- 轮播图 -->
+            <div class="home-banner">
+                <el-carousel height="500px">
+                    <el-carousel-item v-for="item in bannerList" :key="item.id">
+                        <img :src="item.imgUrl" alt="">
+                    </el-carousel-item>
+                </el-carousel>
+            </div>
         </div>
     </div>
 </template>
 
 
 <style scoped lang="scss">
+.home-banner {
+    width: 1240px;
+    height: 500px;
+    margin: 0 auto;
+
+    img {
+        width: 100%;
+        height: 500px;
+    }
+}
+
 .top-category {
     h3 {
         font-size: 28px;
