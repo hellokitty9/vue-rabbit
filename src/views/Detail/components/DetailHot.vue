50b4d3c3230e3b927f3facd429842c2bdf658468
hellokitty9 <1836174807@qq.com>
2023-08-13 16:43:51 +0800

    详情热榜-title和列表内容的适配

--- src/views/Detail/components/DetailHot.vue
+++ src/views/Detail/components/DetailHot.vue
@@ -1,8 +1,24 @@
 <script setup>
 // 24小时热榜获取数据渲染模板
 import { fetchHotGoodsAPI } from '@/apis/detail'
-import { onMounted, ref } from "vue"
-import { useRoute } from 'vue-router';
+import { computed, onMounted, ref } from "vue"
+import { useRoute } from 'vue-router'
+// 设计props参数 适配不同的title和数据
+
+const props = defineProps({
+    hotType: {
+        type: Number
+    }
+})
+
+// 适配title  1- 24小时热榜  2- 周热榜
+const TYPEMAP = {
+    1: '24小时热榜',
+    2: '周热榜'
+}
+const title = computed(() => TYPEMAP[props.hotType])
+
+
 // 1. 封装接口
 // 2. 调用接口渲染模板
 const hotList = ref([])
@@ -10,7 +26,7 @@ const route = useRoute()
 const getHotList = async () => {
     const res = await fetchHotGoodsAPI({
         id: route.params.id,
-        type: 1
+        type: props.hotType
     })
     hotList.value = res.result
 }
@@ -20,7 +36,7 @@ onMounted(() => getHotList())
 
 <template>
     <div class="goods-hot">
-        <h3>周日榜单</h3>
+        <h3>{{ title }}</h3>
         <!-- 商品区块 -->
         <RouterLink to="/" class="goods-item" v-for="item in hotList" :key="item.id">
             <img :src="item.picture" alt="" />
