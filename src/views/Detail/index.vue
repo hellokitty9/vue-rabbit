0398165c0ff252831a3b6c01e44e42ac4eea22b4
hellokitty9 <1836174807@qq.com>
2023-08-13 20:41:49 +0800

    SKU组件

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import DetailHot from './components/DetailHot.vue'
 import ImageView from '@/components/ImageView/index.vue'
+import XtxSku from '@/components/XtxSku/index.vue'
 import { getDetail } from "@/apis/detail"
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
@@ -83,7 +84,7 @@ onMounted(() => getGoods())
                                 </dl>
                             </div>
                             <!-- sku组件 -->
-
+                            <XtxSku :goods="goods" />
                             <!-- 数据组件 -->
 
                             <!-- 按钮组件 -->
