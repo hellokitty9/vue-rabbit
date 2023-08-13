6a322c1408cff5ccf24de0f30ccca791ca931a2e
hellokitty9 <1836174807@qq.com>
2023-08-13 21:05:32 +0800

    详情页-全局组件插件化

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+// 将components中的所有组件进行全局化注册
+// 通过插件的方式
+import ImageView from './ImageView/index.vue'
+import Sku from './XtxSku/index.vue'
+export const componentPlugin = {
+    install(app) {
+        //app.component('组件名称', 组件配置对象)
+        app.component('XtxImageView', ImageView)
+        app.component('XtxSku', Sku)
+    }
+}
--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ import '@/styles/common.scss'
 // 引入懒加载指令插件并注册
 import { lazyPlugin } from '@/directives'
 
+// 引入全局组件插件
+import { componentPlugin } from '@/components'
+
 // // 测试接口函数
 // import { getCategory } from "@/apis/testAPI";
 // getCategory().then(res => {
@@ -20,4 +23,5 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
+app.use(componentPlugin)
 app.mount('#app')
