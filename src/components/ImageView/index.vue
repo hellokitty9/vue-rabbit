bd9b3a0cdd789c924b343e5c8c233f5e9ec51071
hellokitty9 <1836174807@qq.com>
2023-08-13 18:10:19 +0800

    图片预览组件-滑块跟随鼠标移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue'
+import { watch, ref } from 'vue'
+import { useMouseInElement } from '@vueuse/core';
 // 图片列表
 const imageList = [
     "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -14,6 +15,41 @@ const activeIndex = ref(0)
 const enterhandler = (i) => {
     activeIndex.value = i
 }
+
+// 2. 获取鼠标的相对位置
+const target = ref(null)
+const { elementX, elementY, isOutside } = useMouseInElement(target)
+
+// 3. 控制滑块跟随鼠标移动 (监听elementX/Y变化, 一旦变化 重新设置left/top)
+const left = ref(0)
+const top = ref(0)
+watch([elementX, elementY], () => {
+    // 有效范围内控制滑块
+    // 横向
+    if (elementX.value > 100 && elementX.value < 300) {
+        left.value = elementX.value - 100
+    }
+    // 纵向
+    if (elementY.value > 100 && elementY.value < 300) {
+        top.value = elementY.value - 100
+    }
+
+    // 处理边界
+    if (elementX.value > 300) {
+        left.value = 200
+    }
+    if (elementX.value < 100) {
+        left.value = 0
+    }
+
+    if (elementY.value > 300) {
+        top.value = 200
+    }
+    if (elementY.value < 100) {
+        top.value = 0
+    }
+})
+
 </script>
 
 
@@ -23,7 +59,7 @@ const enterhandler = (i) => {
         <div class="middle" ref="target">
             <img :src="imageList[activeIndex]" alt="" />
             <!-- 蒙层小滑块 -->
-            <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+            <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
         </div>
         <!-- 小图列表 -->
         <ul class="small">
