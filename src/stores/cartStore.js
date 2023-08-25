ac3786f851d53ff5619da4de48257fd1e61bb878
hellokitty9 <1836174807@qq.com>
2023-08-25 23:19:21 +0800

    本地-头部购物车-计算逻辑

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,7 +1,7 @@
 // 封装购物车模块
 
 import { defineStore } from "pinia"
-import { ref } from "vue"
+import { computed, ref } from "vue"
 
 
 export const useCartStore = defineStore('cart', () => {
@@ -28,10 +28,19 @@ export const useCartStore = defineStore('cart', () => {
         const idx = cartList.value.findIndex((item) => skuId === item.skuId)
         cartList.value.splice(idx, 1)
     }
+
+    //计算属性
+    // 1. 总的数量 所有项的count之和
+    const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
+    // 2. 总价 所有项的count*price之和
+    const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
+
     return {
         cartList,
         addCart,
-        delCart
+        delCart,
+        allCount,
+        allPrice
     }
 },
     {
--- src/views/Layout/components/HeaderCart.vue
+++ src/views/Layout/components/HeaderCart.vue
@@ -25,14 +25,14 @@ const cartStore = useCartStore()
                             <p class="count">x{{ i.count }}</p>
                         </div>
                     </RouterLink>
-                    <i class="iconfont icon-close-new" @click="store.delCart(i.skuId)"></i>
+                    <i class="iconfont icon-close-new" @click="cartStore.delCart(i.skuId)"></i>
                 </div>
 
             </div>
             <div class="foot">
                 <div class="total">
-                    <p>共 10 件商品</p>
-                    <p>&yen; 100.00 </p>
+                    <p>共 {{ cartStore.allCount }} 件商品</p>
+                    <p>&yen; {{ cartStore.allPrice }} </p>
                 </div>
                 <el-button size="large" type="primary">去购物车结算</el-button>
             </div>
